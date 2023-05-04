17bd3ff987480013082d80993b013a655aeee76b
Victor Johnson <95552161+realmichealbay@users.noreply.github.com>
2023-05-03 19:06:43 -0500

    On branch master  Changes to be committed: 	deleted:    json.json 	modified:   umad/test.py

--- umad/test.py
+++ umad/test.py
@@ -3,27 +3,25 @@ import json
 
 result = subprocess.run(['tasklist'],stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
 
+final_list = {}
 if result.returncode == 0:
     lines = result.stdout.splitlines()
-    task_list = {}
-    final_list = []
-    x = 1
+    print("start")
     for line in lines[3:]:
+        task_list = {}
         task_list["Image Name"] = line.split(maxsplit=5)[0]
+        image_name = task_list["Image Name"] = line.split(maxsplit=5)[0]
         task_list["PID"] = line.split(maxsplit=5)[1]
         task_list["Session Name"] = line.split(maxsplit=5)[2]
         task_list["Session#"] = line.split(maxsplit=5)[3]
         task_list["Mem Usage"] = line.split(maxsplit=5)[4]
-        final_list.update()
-        final_list[f"{str(x)}"] = task_list
-        x=x+1
+        final_list[f"Process: {image_name}"] = task_list
 
-    print(final_list)
-    
+    print("done")
     with open("json.json","w") as file:
         file.write(json.dumps(final_list))
         file.close()
-    
+
 else:
     print(f"Error {result.stderr}")
 
